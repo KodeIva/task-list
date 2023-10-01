d36d0ecb4aeeebec8f53a301d565f2dbad90619c
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-10-01 16:02:19 +0100

    Added handleSubmit function, onSubmit and onChange function

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [newTask,setNewTask] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTodos([...todos,{id: crypto.randomUUID,title: newTask, completed: false}])
+  }
+
+  console.log(todos);
 
   return (
     <>
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="item">New Task</label>
-        <input type="text" />
+        <input defaultValue={newTask} onChange={e => setNewTask(e.target.value)}  type="text" />
       </div>
-      <button>Add</button>
+      <button >Add</button>
     </form>
     <h1>Tasks: </h1>
     </>
