5af3e0599bb2857af4ddffc0da719ac9fbf50f12
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-02-05 17:14:05 +0000

    input, add button and task div - styled

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
     if(newTask) {
       const newAddedTask = {id: uuidv4(), title:newTask}
       setTodos([...todos,newAddedTask]) 
-
     }
     if(!newTask) {
       toast.error('Please add task')
@@ -48,29 +47,30 @@ function App() {
 
   return (
     <>
-    <div className='bg-indigo-400 flex flex-col items-center w-[100%] h-[100vh]' >
-    <form className=' flex flex-col items-center mt-[10px] h-[auto] md:bg-orange-500 md:flex-row md:w-[70%] lg:w-[50%] w-[90%] p-[50px]' onSubmit={addNewTask}>
-      <div>
+    <div className='bg-green-50 flex flex-col items-center w-[100%] h-[100vh]' >
+    <form className=' flex flex-col items-center mt-[10px] h-[auto] w-[90%] p-[50px]' onSubmit={addNewTask}>
+      <div className='flex flex-col items-center bg-white lg:flex-row'>
         <input 
-          className='w-[350px] m-2 p-2 rounded-lg'
+          className='w-[350px] m-2 p-2 rounded-lg bg-blue-100 text-white'
           value={newTask} placeholder='Add new task ...' onChange={e => setNewTask(e.target.value)} id='task' type="text" />
+        <button className='bg-blue-100 m-2 text-white flex justify-center rounded-lg items-center  w-[350px] h-9 text-3xl' onClick={(e)=> addNewTask(e)}><BiPlus className='text-slate-500 mt-1'/></button>
       </div>
-      <button className='bg-white flex justify-center rounded-lg items-center  w-[350px] h-9 text-3xl' onClick={(e)=> addNewTask(e)}><BiPlus className='text-slate-500 mt-1'/></button>
+      
     </form>
     
     {
       todos.length > 0 &&  
        <div >
         <h1>Tasks: </h1>
-        <div className="taskList" style={{background:'pink'}}>
+        <div className="taskList bg-blue-200 rounded-lg text-gray-600 w-[350px] lg:w-[450px]">
          {todos.map(todo => {
           const {title,id} = todo
           return (
-            <div className='singleItem' key={id}>
-              <input type='checkbox' value={completed} checked={completed} onChange={() => completedTask(!completed)}/>
-              <h3 style={{textDecoration: completed ? 'line-through' : 'none'}}>{title}</h3>
+            <div className='singleItem flex relative w-[100%]'  key={id}>
+              <input type='checkbox' className='m-2' value={completed} checked={completed} onChange={() => completedTask(!completed)}/>
+              <h3 style={{textDecoration: completed ? 'line-through' : 'none'}} className='m-2 left-9'>{title}</h3>
               
-              <button className="delete" onClick={() => deletedTask(id)} >Delete Item</button>
+              <button className="delete bg-blue-800 my-2 mx-4 px-4 absolute right-0" onClick={() => deletedTask(id)} >Delete</button>
             </div>
           )
          })}
